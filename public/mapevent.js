e851cb509ed0825186d3d040b4cbf3c30d092adf
ricardoul <ricardoul@hotmail.com>
2016-11-10 19:36:42 -0300

    Mapa y picks

--- public/mapevent.js
+++ public/mapevent.js
@@ -4,4 +4,25 @@ angular.module('EventCMS')
     "$scope", "$state", "$log", "$timeout", "$firebaseArray", "alertsManager", "$rootScope", "$stateParams", 
     function($scope, $state, $log, $timeout, $firebaseArray, alertsManager, $rootScope, $stateParams)  {
 
+         //save the ID of the event to edit that was passed from the list view
+        var editEventId = $stateParams.passId;
+        $log.info("stateparms", editEventId);
+        // var userId = firebase.auth().currentUser.uid;
+        $scope.userId = "laca"
+        $scope.fireBaseRef = firebase.database().ref('/events')
+        
+        function updateEvents(){
+
+         $scope.fireBaseRef.once('value').then(function(snapshot) {
+                var arr = _.values(snapshot.val());
+                $scope.events = arr
+                $scope.$apply()
+              // ...
+            });
+       }
+
+        updateEvents()
+
+        $scope.events = [
+        ]
     }]);
